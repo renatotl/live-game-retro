0b417f103fac58854d6706d9ac80e8a5e88445d6
Renato Tenório Lino <103604513+renatotl@users.noreply.github.com>
2022-11-22 15:39:41 -0300

    formatação

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,17 +1,20 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+// import { AppController } from './app.controller';
+// import { AppService } from './app.service';
 
 //decorator tendo o  controller e service
-// o controller é o cara que controla as rotas 
+// o controller é o cara que controla as rotas
 // service trabalha com o banco e oferece conteúdo
 
 // o decorate segue o Open Closed Principle/ ele altera o comportamento de algo na class sem precisar mecher no code/ estamos usando os decoratos do NEST, mas podemos criar os nossos
+/*ANTIGO
 @Module({
   imports: [],// listta
   controllers: [AppController],// lista/ se tiver outro controller ele tbm recebe
   providers: [AppService],// lista / provendo para o controller
   // uma cass dentro de outra
 })
+*/
+@Module({})
 export class AppModule {}
-// o appModule que junto todo mundo 
+// o appModule que junto todo mundo
