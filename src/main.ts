4197dc30d9742d280fd496b93b02c722bf728981
Renato Tenório Lino <103604513+renatotl@users.noreply.github.com>
2022-11-23 16:37:02 -0300

    feat: swagger na main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 //criando a aplicação através do AppModule
 // uma função async vai rodar pra sempre no terminal
@@ -7,6 +9,19 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   // o NestFactory automaticamente coloca o service dentro do controller
   const app = await NestFactory.create(AppModule);
+
+  // validate
+  app.useGlobalPipes(new ValidationPipe());
+
+  const config = new DocumentBuilder()
+    .setTitle('Live Game Retro')
+    .setDescription('Aplicação para cadastramento de novos games')
+    .setVersion('1.0.0')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
