cba4bacccb0c4c6940019db5588def2d5e231d24
Renato Tenório Lino <103604513+renatotl@users.noreply.github.com>
2022-11-22 21:23:40 -0300

    fix: UserDto

--- src/user/service/dto/userInput.ts
+++ src/user/service/dto/userInput.ts
@@ -1,27 +1,29 @@
 // DTO = data trasport object
 
-
-import { ApiProperty } from "@nestjs/swagger"// yarn add @nestjs/swagger
-import { IsEmail, IsNotEmpty, IsString } from "class-validator"
-
+import { ApiProperty } from '@nestjs/swagger'; // yarn add @nestjs/swagger
+import { IsEmail, IsNotEmpty, IsString } from 'class-validator';
 
 export class UserDto {
-   // só não tem o id
+  // só não tem o id
 
-   @ApiProperty()
-   @IsString()
-    name: string;
-    @ApiProperty()
-    @IsEmail()
-    @IsNotEmpty()
-    email: string;
-    @ApiProperty()
-    @IsString()
-    password: string;
-    @ApiProperty()
-    @IsString()
-    cpf: string; // cpf só pode ser string / existe CPF que tem o 0 na frente e o banco desconsidera esse cara!
-    @ApiProperty()
-    @IsString()
-    role: string;
+  @ApiProperty()
+  @IsString()
+  @IsNotEmpty()
+  name: string;
+  @ApiProperty()
+  @IsEmail()
+  @IsNotEmpty()
+  email: string;
+  @ApiProperty()
+  @IsString()
+  @IsNotEmpty()
+  password: string;
+  @ApiProperty()
+  @IsString()
+  @IsNotEmpty()
+  cpf: string; // cpf só pode ser string / existe CPF que tem o 0 na frente e o banco desconsidera esse cara!
+  @ApiProperty()
+  @IsString()
+  @IsNotEmpty()
+  role: string;
 }
