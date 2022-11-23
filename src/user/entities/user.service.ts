25715b3e9f18d84ffac9a78ba008ba0820f815e2
Renato Tenório Lino <103604513+renatotl@users.noreply.github.com>
2022-11-22 21:19:43 -0300

    feat: updateUser

--- src/user/entities/user.service.ts
+++ src/user/entities/user.service.ts
@@ -1,23 +1,36 @@
-import { UserDto } from "../service/dto/userInput";
-import { IUserEntity } from "./user.entity";
-import { randomUUID
- } from "node:crypto";
+import { UserDto } from '../service/dto/userInput';
+import { IUserEntity } from './user.entity';
+import { randomUUID } from 'node:crypto';
+import { PartialUserDto } from '../service/dto/partialUserInput.Dto';
 export class UserService {
-
-    private users: IUserEntity[] = []// uma lista e iniciand como vazio
-    // para criar um usuário vou precisar receber um user e vamos retornar uma promise da entitade que criamos 
+  private users: IUserEntity[] = []; // uma lista e iniciand como vazio
+  // para criar um usuário vou precisar receber um user e vamos retornar uma promise da entitade que criamos
   async createUser(user: UserDto): Promise<IUserEntity> {
-// recebe tudo do user mais um id gerado pelo node
-    const userEntity = {...user, id: randomUUID()}
-   this.users. push(userEntity)// recebe o user como parametro e coloca na lista users
-   return userEntity
+    // recebe tudo do user mais um id gerado pelo node
+    const userEntity = { ...user, id: randomUUID() };
+    this.users.push(userEntity); // recebe o user como parametro e coloca na lista users
+    return userEntity;
 
-   //return Promise.revolce(userEntity)  outra forma de fazer caso eu não tenha o async em cima
-   }
+    //return Promise.revolce(userEntity)  outra forma de fazer caso eu não tenha o async em cima
+  }
 
-   async updateUser(user: UserDto): Promise<IUserEntity> {
-    
-   }
+  async updateUser(userData: PartialUserDto): Promise<IUserEntity> {
+    // faz um map em users ( usuário criado)
+    this.users.map((user, index) => {
+      // pegando o user eo index
+      // user.id que o map passou for exatamente igual a userData.id
+      if (user.id === userData.id) {
+        /// o usuário atualizado vai receber um user só
+        const updatedUser = Object.assign(user, userData); // nosso taget ou auvo é "user" o que ele vai buscar no user e alterar o userData/ então os dados que vier do meu userData vou alterar do usuário/ retorna um objeto alterado
+
+        // depois de pegar o user e index vamon interar nessa lista
+        this.users.splice(index, 1, updatedUser); // index é a posição inicial, quantos elementos vou remover no caso é 1/ o que vou colocar no lugar? o próprio objeto atualizao o updatedUser
+      }
+    });
+    // retonra esse cara
+    const updatedUser = this.users.find((user) => user.id === userData.id);
+    return updatedUser;
+  }
 }
 
 // foi feito uma interface IUserEntity, mas o usuário não pode criar o próprio id
---
+++ src/user/service/dto/partialUserInput.Dto.ts
@@ -0,0 +1,7 @@
+import { PartialType } from "@nestjs/mapped-types"
+import { UserDto } from "./userInput"
+
+// faz com que eu reaproveite tudo do meu UserDto, MAS TUDO OPCIONAL/ se vier 2 ok se vier tudo ok tbm
+export class PartialUserDto extends PartialType(UserDto) {
+ id: string
+}
