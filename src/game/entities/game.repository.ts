7114f814c7f0c4fbe5ec906498f2905f868fee44
Renato Tenório Lino <103604513+renatotl@users.noreply.github.com>
2022-11-29 22:15:45 -0300

    feat: gameRepository

--- src/game/entities/game.repository.ts
+++ src/game/entities/game.repository.ts
@@ -1,7 +1,42 @@
 import { Injectable } from "@nestjs/common";
+import { PartialGameDto } from "src/user/service/dto/partialGameInput.Dto";
+import { IGamesEntity } from "./games.entity";
 
 
 @Injectable()
 export class GameRepository {
 
+    constructor( private readonly primsa: PrismaService){}
+
+    async create(game:IGamesEntity): Promise<IGamesEntity>{
+      const createdGame = await this.primsa.game.create({data: game})
+      return createdGame
+    }
+
+    async updateGame(game: PartialGameDto): Promise<IGamesEntity>{
+     const updatedGame = await this.primsa.game.update({
+        where: {id: game.id},
+        data: game
+     })
+     return updatedGame
+    }
+
+async deleteGame(id: string): Promise<IGamesEntity>{
+    const deletedGame = await this.primsa.game.delete({
+        where: { id: id}
+    })
+    return deletedGame
+}
+
+async getAllGames(): Promise<IGamesEntity[]>{
+  return await this.primsa.game.findMany()
+}
+
+async findGameById(id:string): Promise<IGamesEntity>{
+   const foundGame = await this.primsa.game.findUniqueOrThrow({
+    // onde
+    where: { id: id },
+  });
+return foundGame
+}
 }
