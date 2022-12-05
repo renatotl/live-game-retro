034ecc1b30f481af140247d4a86fde13467be1af
Renato Tenório Lino <103604513+renatotl@users.noreply.github.com>
2022-12-05 16:42:36 -0300

    fix: partialprofiledto

--- src/profile/dto/update-profile.dto.ts
+++ src/profile/dto/update-profile.dto.ts
@@ -1,4 +1,6 @@
 import { PartialType } from '@nestjs/swagger';
 import { CreateProfileDto } from './create-profile.dto';
 
-export class PartialProfileDto extends PartialType(CreateProfileDto) {}
+export class PartialProfileDto extends PartialType(CreateProfileDto) {
+    id: string
+}
