8ee3541bc1d6b7f6b837c377b5d2cd5b0fc5d561
Renato Tenório Lino <103604513+renatotl@users.noreply.github.com>
2022-12-06 16:20:56 -0300

    feat: genero controler

--- src/genero/genero.controller.ts
+++ src/genero/genero.controller.ts
@@ -1,34 +1,91 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import {BadRequestException, Controller, Get, Post, Body, Patch, Param, Delete,Res } from '@nestjs/common';
 import { GeneroService } from './genero.service';
 import { CreateGeneroDto } from './dto/create-genero.dto';
-import { UpdateGeneroDto } from './dto/update-genero.dto';
+import { PartialGeneroDto } from './dto/update-genero.dto';
+import { Response } from 'express';
+import { ApiTags } from '@nestjs/swagger';
 
+
+@ApiTags('genero')
 @Controller('genero')
 export class GeneroController {
   constructor(private readonly generoService: GeneroService) {}
 
-  @Post()
-  create(@Body() createGeneroDto: CreateGeneroDto) {
-    return this.generoService.create(createGeneroDto);
+  @Post('createGenero')
+  async createGenero(
+    @Body() { title }: CreateGeneroDto,
+    @Res() response: Response,
+  ): Promise<void> {
+    // de IUserEntity troquie para void porque não tem return
+    // se algo pode dar errado vai da errado. clen code
+    try {
+      const result = await this.generoService.create({
+        title
+      });
+
+      response.status(201).send(result);
+    } catch (err) {
+      console.log(err);
+      response.status(400);
+      throw new BadRequestException(err.message); // mensgem vem do service
+    }
   }
 
-  @Get()
-  findAll() {
-    return this.generoService.findAll();
+  @Get('getAllGenero')
+  async getAllGenero(@Res() response: Response): Promise<void> {
+    // mesmo método do service
+    try {
+      const result = await this.generoService.findAll(); // esse ultime getAllUsers() é a função do banco
+      response.status(201).send(result);
+    } catch (err) {
+      console.log(err);
+      response.status(400);
+      throw new BadRequestException(err.message); // mensgem vem do service
+    }
   }
 
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.generoService.findOne(+id);
+  @Get('getGeneroById/:id')
+  async getGeneroById(
+    @Param('id') generoId: string,
+    @Res() response: Response,
+  ): Promise<void> {
+    try {
+      const result = await this.generoService.findOne(generoId);
+      response.status(201).send(result);
+    } catch (err) {
+      console.log(err);
+      throw new BadRequestException(err.message); // mensgem vem do service
+    }
   }
 
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateGeneroDto: UpdateGeneroDto) {
-    return this.generoService.update(+id, updateGeneroDto);
+  @Patch('updateGenero')
+  async updateProfile(
+    @Body() generoData: PartialGeneroDto,
+    @Res() response: Response,
+  ): Promise<void> {
+    try {
+      const result = await this.generoService.update(generoData);
+      response.status(201).send(result);
+    } catch (err) {
+      console.log(err);
+      response.status(400);
+      throw new BadRequestException(err.message); // mensgem vem do service
+    }
   }
 
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.generoService.remove(+id);
+  @Delete('deleteGeneroById/:id')
+  async deleteGeneroById(@Param('id') generoId: string): Promise<String> {
+    // promise de string
+    try {
+      const generoIdDeleted = await this.generoService.remove(generoId);
+      console.log(generoIdDeleted);
+      if (generoIdDeleted) {
+        return 'Género deletado com sucesso';
+      } else {
+        return 'Género não encontrado';
+      }
+    } catch (err) {
+      console.log(err);
+    }
   }
 }
