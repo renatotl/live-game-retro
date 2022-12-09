400d58a13a388f458ba9b5eb676ccadd10d2ecf0
Renato Tenório Lino <103604513+renatotl@users.noreply.github.com>
2022-12-09 17:39:54 -0300

    feat: auth service

---
+++ src/auth/auth.service.ts
@@ -0,0 +1,44 @@
+import {
+    Injectable,
+    NotFoundException,
+    UnauthorizedException,
+  } from '@nestjs/common';
+  import { JwtService } from '@nestjs/jwt';
+  import { PrismaService } from 'src/prisma/prisma.service';
+  import { LoginResponseDto } from './dto/loggin-response.dto';
+  import { LoginDto } from './dto/loggin.dto';
+  import * as bcrypt from 'bcrypt'; //npm i bcrypt / npm i -D @types/bcrypt
+  
+  @Injectable()
+  export class AuthService {
+    constructor(
+      private prismaService: PrismaService,
+      private jwtService: JwtService,
+    ) {}
+  
+    async login(data: LoginDto): Promise<LoginResponseDto> {
+      const { email, password } = data;// descontruimos
+  
+      const userExists = await this.prismaService.user.findUnique({
+        where: { email },
+      });
+  
+      if (!userExists) {
+        throw new NotFoundException('Credenciais inválidas');
+      }
+  
+      // o compare é um método do bcrypt
+      const isHashValid = await bcrypt.compare(password, userExists.password);
+  
+      if (!isHashValid) {
+        throw new UnauthorizedException('Credenciais inválidas');
+      }
+  
+      delete userExists.password;// na hora do retorno ele não traz a senha
+  
+      return {
+        token: this.jwtService.sign({ email }),// asina o token com o email que estamos passando
+        user: userExists,// retorna o usuário
+      };
+    }
+  }
