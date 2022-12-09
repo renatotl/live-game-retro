bb48f86da8ac395ad73bae940798bd29b19f84e3
Renato Tenório Lino <103604513+renatotl@users.noreply.github.com>
2022-12-08 22:51:15 -0300

    feat: loggin dto

---
+++ src/auth/dto/loggin.dto.ts
@@ -0,0 +1,22 @@
+import { ApiProperty } from '@nestjs/swagger';
+import { IsEmail, IsNotEmpty, IsString } from 'class-validator';
+
+export class LoginDto {
+  @IsString()
+  @IsNotEmpty()
+  @ApiProperty({
+    description: 'Email para cadastro',
+    example: 'Abcd@1234.com',
+  }) //swagger
+  @IsEmail()
+  email: string;
+
+  @IsString()
+  @IsNotEmpty()
+  @ApiProperty({
+    description: 'Exemplo de senha',
+    example: 'senhaFraca',
+  }) //swagger
+  @IsString()
+  password: string;
+}
